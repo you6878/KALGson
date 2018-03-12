1226c72b06c9e9d7d251f3e077869473a6b54684
유명식 <myeongsic@MacBook-Pro.local>
2018-03-12 23:11:54 +0900

    init

---
+++ app/src/main/java/com/bpplatform/howlgson/UserDTO.kt
@@ -0,0 +1,11 @@
+package com.bpplatform.howlgson
+
+/**
+ * Created by myeongsic on 2018. 3. 12..
+ */
+
+class UserDTO {
+    var name: String? = null
+    var age: Int = 0
+    var city: String? = null
+}
